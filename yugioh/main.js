b23ae28a19296df350ced95399e53c7b02a0e830
Joao Henrique <joao.henrique1299@hotmail.com>
2023-02-11 05:44:02 -0300

    Prototype of function to create twin cards + function destroy_card + avoid user to try select text/image

--- yugioh/main.js
+++ yugioh/main.js
@@ -1,5 +1,5 @@
 function add_listeners(){
-    var label = document.querySelectorAll("label.card");
+    let label = document.querySelectorAll("label.card");
 
 
     for (l of label)
@@ -12,4 +12,16 @@ function add_listeners(){
 function flip_card(e) {
     img = this.querySelector('img[alt=back]');
     img.style.visibility = (img.style.visibility === 'hidden') ? 'visible' : 'hidden';
+}
+
+function clone_cards(){
+    console.log("clonning.");
+}
+
+function destroy_card (card_label) {
+    id_checkbox = card_label.getAttribute('for');
+    checkbox = document.querySelector(`input#${id_checkbox}`);
+
+    checkbox.remove();
+    card_label.remove();
 }
